00e68523fdd0f8935e6f4bfd865535a4dfbf1cbb
teqnika <teqnika@gmail.com>
2022-03-23 20:42:55 +0900

    Update Setup shell

--- setup_my_env.sh
+++ setup_my_env.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# Install git and zsh
+sudo apt install -y git zsh
+
+# Install zpresto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Make Directory For git repositories
+mkdir ~/repos 
+cd ~/repos
+
+# Clone from 
+git clone git@github.com:teqnika/setup_shell_env.git
+
 # Delete zsh_dot_file and Create new alias(Symlink)
 
 zsh_config_files=("zshrc" "zlogin" "zlogout" "zpreztorc" "zprofile" "zshenv")
-
-# Delete 
-
-for e in ${zsh_config_files[@]}; do
-  unlink ~/.${e}
-done
 
 
 for e in ${zsh_config_files[@]}; do
